189a23d8c59ad3950d65b79be6c3b4abae1cd7eb
yuliiaishchenko <romashka.ukr@gmail.com>
2023-03-18 15:47:40 +0200

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const INTERVAL_TIME = 1000;
+let intervalId = null;
 
+const refs = {
+    body: document.querySelector('body'),
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+}
+
+refs.btnStart.addEventListener('click', changeColor);
+refs.btnStop.addEventListener('click', onBtnStop);
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+  function changeColor (){
+    refs.btnStart.disable = true;
+    refs.btnStop.disable = false;
+
+    intervalId = setInterval(() =>{
+        refs.body.style.backgroundColor = getRandomHexColor()
+    }, INTERVAL_TIME)
+  };
+
+  function onBtnStop(){
+    refs.btnStart.disable = false;
+    refs.btnStop.disable = true;
+    clearInterval(intervalId);
+  };
